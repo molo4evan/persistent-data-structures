a7bd51e6ab79312ac47a3ea4636a1d4eeae72fd5
Ivan Molochev <i.molochev@g.nsu.ru>
2020-12-25 23:42:32 +0700

    Initial commit: project structure

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.21"
+}
+
+group = "ru.nsu.mmp.pds"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
---
+++ src/main/kotlin/ru/nsu/mmp/pds/Main.kt
@@ -0,0 +1,5 @@
+package ru.nsu.mmp.pds
+
+fun main(args: Array<String>) {
+    println("Let's create some Persistent Data Structures!")
+}
